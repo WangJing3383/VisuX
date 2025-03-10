367589db879597251fb67015ba598afb7e370088
slaymyway <fluttershymlp.3mlp@gmail.com>
2025-03-10 23:49:02 +0100

    Graph Style Test

---
+++ my-visualization-app/frontend/src/components/graph/GraphStyle.test.js
@@ -0,0 +1,38 @@
+import GraphStyle from './GraphStyle';
+
+describe('GraphStyle Class', () => {
+  let graphStyle;
+
+  beforeEach(() => {
+    graphStyle = new GraphStyle();
+  });
+
+  test('should initialize with default values', () => {
+    expect(graphStyle.colorScheme).toBe('#0000FF');
+    expect(graphStyle.markerStyle).toEqual({ size: 8, color: '#0000FF' });
+    expect(graphStyle.lineStyle).toEqual({ width: 2, dash: 'solid' });
+    expect(graphStyle.layoutSize).toEqual({ width: 600, height: 400 });
+    expect(graphStyle.backgroundColor).toBe('#FFFFFF');
+  });
+
+  test('should set a new color scheme', () => {
+    const newColorScheme = '#FF0000';
+    graphStyle.setColorScheme(newColorScheme);
+
+    expect(graphStyle.colorScheme).toBe(newColorScheme);
+    expect(graphStyle.markerStyle.color).toBe(newColorScheme);
+  });
+
+  test('should change color using changeColor method', () => {
+    const newColor = '#00FF00';
+    graphStyle.changeColor(newColor);
+
+    expect(graphStyle.colorScheme).toBe(newColor);
+    expect(graphStyle.markerStyle.color).toBe(newColor);
+  });
+
+  test('should return the current marker style', () => {
+    const markerStyle = graphStyle.getMarkerStyle();
+    expect(markerStyle).toEqual({ size: 8, color: '#0000FF' });
+  });
+});
