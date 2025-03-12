3f7d3adf457cac16076eb48654ce9e96d344f89d
slaymyway <fluttershymlp.3mlp@gmail.com>
2025-03-12 02:52:50 +0100

    Header Test

--- my-visualization-app/frontend/src/components/homepage/Header.js
+++ my-visualization-app/frontend/src/components/homepage/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Layout, Button } from 'antd';
+import { Layout } from 'antd';
 import FileComponent from "../file/FileComponent";
 
 const { Header } = Layout;
 
 const HeaderNav = ({ uiController }) => {
   return (
-      <Header style={{background: '#fff', padding: '0 20px', display: 'flex', justifyContent: 'space-between'}}>
-          <div style={{margin: '-20px'}}>
+      <Header data-testid="header-nav" style={{background: '#fff', padding: '0 20px', display: 'flex', justifyContent: 'space-between'}}>
+          <div data-testid="header-title" style={{margin: '-20px'}}>
               <h2>Visux</h2>
           </div>
-          <div style={{marginLeft: "auto"}}>
+          <div  data-testid="file-component-container" style={{marginLeft: "auto"}}>
               <FileComponent uiController={uiController}/>
           </div>
       </Header>
---
+++ my-visualization-app/frontend/src/components/homepage/Header.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import HeaderNav from './Header';
+
+// Mock the dependent modules
+jest.mock('../file/FileComponent', () => () => (
+  <div data-testid="mock-file">Mocked FileComponent</div>
+));
+
+
+describe('HeaderNav', () => {
+  let uiControllerMock;
+
+  // Create a new instance of UIController before each test
+  beforeEach(() => {
+    uiControllerMock = { uiControllerAction: jest.fn() };
+    jest.clearAllMocks();
+  });
+
+  test('should render HeaderNav correctly', () => {
+    render(<HeaderNav uiController={uiControllerMock} />);
+
+    expect(screen.getByText('Visux')).toBeInTheDocument();
+    expect(screen.getByTestId('header-nav')).toBeInTheDocument();
+  });
+
+  test('should have the correct styles applied to Header', () => {
+    render(<HeaderNav uiController={uiControllerMock} />);
+
+    //Check if the header has correct styles
+    const headerElement = screen.getByRole('banner') || screen.getByTagName('header');
+    expect(headerElement).toHaveStyle('background: #fff');
+    expect(headerElement).toHaveStyle('padding: 0 20px');
+  });
+
+  test('should render FileComponent inside HeaderNav', () => {
+    render(<HeaderNav uiController={uiControllerMock} />);
+
+    //Check if FileComponent was mounted
+    expect(screen.getByTestId('mock-file')).toBeInTheDocument();
+  });
+});
